d958393d34f02003b4361ce8e3b0b5f7c6f54b39
rjmarques <ricardomarques377@gmail.com>
2024-08-03 21:00:12 +0100

    expose files

--- main.tf
+++ main.tf
@@ -7,27 +7,27 @@ terraform {
   }
 }
 
-# # List all files in the static directory
-# locals {
-#   static_files = fileset("${path.module}/dist/", "**/*")
-# }
-
-# output "static_files" {
-#   value = local.static_files
-# }
-
-# output "static_file_paths" {
-#   value = [for file in local.static_files : "${path.module}/dist/${file}"]
-# }
-
-variable "bucket" {
-  description = "the bucket to where the files will be uploaded"
+# List all files in the static directory
+locals {
+  static_files = fileset("${path.module}/dist/", "**/*")
 }
 
-resource "aws_s3_object" "solar_files" {
-  for_each = fileset("${path.module}/dist/", "**/*")
-
-  bucket = var.bucket
-  key    = "site1/${each.key}"
-  source = "${path.module}/dist/${each.key}"
+output "static_files" {
+  value = local.static_files
 }
+
+output "static_file_paths" {
+  value = [for file in local.static_files : "${path.module}/dist/${file}"]
+}
+
+# variable "bucket" {
+#   description = "the bucket to where the files will be uploaded"
+# }
+
+# resource "aws_s3_object" "solar_files" {
+#   for_each = fileset("${path.module}/dist/", "**/*")
+
+#   bucket = var.bucket
+#   key    = "site1/${each.key}"
+#   source = "${path.module}/dist/${each.key}"
+# }
