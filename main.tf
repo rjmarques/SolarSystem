f0f7b1bd3ebb3b531dbf811a620695abdb5d18c5
rjmarques <ricardomarques377@gmail.com>
2024-08-03 20:51:58 +0100

    try to upload directly

--- main.tf
+++ main.tf
@@ -7,15 +7,27 @@ terraform {
   }
 }
 
-# List all files in the static directory
-locals {
-  static_files = fileset("${path.module}/dist/", "**/*")
+# # List all files in the static directory
+# locals {
+#   static_files = fileset("${path.module}/dist/", "**/*")
+# }
+
+# output "static_files" {
+#   value = local.static_files
+# }
+
+# output "static_file_paths" {
+#   value = [for file in local.static_files : "${path.module}/dist/${file}"]
+# }
+
+variable "bucket" {
+  description = "the bucket to where the files will be uploaded"
 }
 
-output "static_files" {
-  value = local.static_files
-}
+resource "aws_s3_object" "solar_files" {
+  for_each = fileset("${path.module}/solar/", "**/*")
 
-output "static_file_paths" {
-  value = [for file in local.static_files : "${path.module}/dist/${file}"]
+  bucket = var.bucket
+  key    = "site1/${each.key}"
+  source = "${path.module}/solar/${each.key}"
 }
